c124443e358a229190974f320b1d688b5bf8a159
Rasel Sharif <raselsharif9@gmail.com>
2023-08-23 11:03:38 +0600

    added toggle menu fuction

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   $('.service__menu').slick({
     infinite: true,
     slidesToShow: 4,
@@ -10,11 +10,17 @@ $(function(){
   });
 })
 
-$(function(){
-      $(".team__icon").click(function(){
-        $(".team__icon__menu").toggleClass("team__icon__menu__active");
-        $(this).toggleClass("team__icon__active");
-        $(".team__overlay").toggleClass("team__overlay__active");
-        $(this).find('i').toggleClass("fa-solid fa-plus fa-solid fa-xmark");
-      })
-})
+// $(function(){
+//       $(".team__icon").click(function(){
+//         $(".team__icon__menu").toggleClass("team__icon__menu__active");
+//         $(this).toggleClass("team__icon__active");
+//         $(".team__overlay").toggleClass("team__overlay__active");
+//         $(this).find('i').toggleClass("fa-solid fa-plus fa-solid fa-xmark");
+//       })
+// })
+
+function toggleClass(toggle) {
+  toggle.parentNode.childNodes[7].classList.toggle('team__icon__menu__active');
+  toggle.parentNode.childNodes[3].classList.toggle('team__overlay__active');
+  toggle.parentNode.childNodes[5].childNodes[1].classList.toggle('fa-xmark');
+}
